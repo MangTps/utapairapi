3bb27aef6b5b2292316e62c95d806b16b082f5d9
MangTps <thanpisit.pisitpon@mail.kmutt.ac.th>
2022-11-23 02:02:40 +0700

    update config.php

--- config.php
+++ config.php
@@ -1,13 +1,11 @@
 <?php
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-echo "Hello there Heroku,MangMang here!";
+//Declare variables
+$host = "us-cdbr-east-06.cleardb.net";
+$user_name = "ba1992da29a591";
+$user_password = "645eb6c1";
+$db_name = "heroku_6b963b71382aa57";
+
+// Connecting to database in phpmyadmin
+$con = mysqli_connect($host,$user_name,$user_password,$db_name) or die ("database connection error");
+
 ?>
