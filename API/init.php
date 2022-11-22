149eb331af1ae91cb7987578b43d9de6933fb91e
MangTps <99586445+MangTps@users.noreply.github.com>
2022-11-23 01:49:37 +0700

    Update init.php

--- API/init.php
+++ API/init.php
@@ -1,11 +1,11 @@
 <?php
 //Declare variables
-$host = "localhost";
-$user_name = "MangMang";
-$user_password = "MangMangcpe327";
-$db_name = "uta_pair";
+$host = "us-cdbr-east-06.cleardb.net";
+$user_name = "ba1992da29a591";
+$user_password = "645eb6c1";
+$db_name = "heroku_6b963b71382aa57";
 
 // Connecting to database in phpmyadmin
-$con = mysqli_connect($host,$user_name,$user_password,$db_name);
+$con = mysqli_connect($host,$user_name,$user_password,$db_name) or die ("database connection error");
 
 ?>
