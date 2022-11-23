e45e0e1c39a976f3f1212b298f57c3566d79da5a
MangTps <thanpisit.pisitpon@mail.kmutt.ac.th>
2022-11-23 18:00:37 +0700

    scoreboardShowBest

--- scoreboardShowBestScore.php
+++ scoreboardShowBestScore.php
@@ -4,10 +4,11 @@ require "init.php";
 $level = $_POST["LEVEL"];
 $username = $_POST["USERNAME"];
 
-$sql = "SELECT S.row_index,S.endTime FROM (SELECT ROW_NUMBER() OVER(ORDER BY score.endTime ) AS row_index , 
-member.username,score.endTime FROM score INNER JOIN member ON score.userId = member.userId 
-WHERE score.modeAndLevelId ='$level' ORDER BY score.endTime) AS S WHERE S.username = '$username'
-ORDER BY S.endTime LIMIT 1";
+$sql = "SET @row_number = 0;";
+$sql2 = "SELECT * FROM (SELECT member.username,score.endTime,(@row_number:=@row_number+1) 
+As row_index FROM score INNER JOIN member ON score.userId = member.userId 
+WHERE score.modeAndLevelId ='$level' ORDER BY score.endTime LIMIT 50) 
+As A WHERE A.username ='$username' LIMIT 1;";
 
 //Declare $result to send data from $sql to database ($con).
 $result = mysqli_query($con, $sql);
