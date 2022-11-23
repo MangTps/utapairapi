9bf3299c5aaffbecf4b410f0f0320f03539eced0
MangTps <thanpisit.pisitpon@mail.kmutt.ac.th>
2022-11-23 17:07:32 +0700

    scoreboardProfile

--- scoreboardProfile.php
+++ scoreboardProfile.php
@@ -4,11 +4,9 @@ require "init.php";
 $level = $_POST["LEVEL"];
 $username = $_POST["USERNAME"];
 $sql = "SET @row_number = 0;";
-$sql2 = "   
-         SELECT S.row_index,S.endTime (SELECT @row_number:=@row_number+1 AS row_index FROM score ORDER BY endtime ,
-         member.username,score.endTime FROM score INNER JOIN member ON score.userId = member.userId 
-         WHERE score.modeAndLevelId ='$level' ORDER BY score.endTime ) AS S WHERE S.username = '$username'
-         ORDER BY S.endTime;";
+$sql2 = "SELECT member.username,score.endTime,(@row_number:=@row_number+1) AS row_index 
+ FROM score INNER JOIN member ON score.userId = member.userId WHERE score.modeAndLevelId ='$level' 
+ AND member.username = '$username' ORDER BY `score`.`endTime` ASC;";
 
 //Declare $result to send data from $sql to database ($con).
 mysqli_query($con, $sql);
