ef3d60347cb35fec17a328aea0d23e3f0ccd3aa4
MangTps <99586445+MangTps@users.noreply.github.com>
2022-11-28 13:34:32 +0700

    comment scoreboard.php

--- scoreboard.php
+++ scoreboard.php
@@ -1,26 +1,29 @@
 <?php
-
+/* include data from init.php */ 
 require "init.php";
-$level = $_POST["LEVEL"];
+$level = $_POST["LEVEL"]; /* declare variable to receive value with string key LEVEL  */
 
 $sql = "SELECT member.username,score.endTime FROM score
         INNER JOIN member ON score.userId = member.userId 
         WHERE score.modeAndLevelId ='$level' 
         ORDER BY score.endTime 
-        LIMIT 50 ";
+        LIMIT 50 "; /* sql command to query top 50 score and endtime in $level */
 
-//Declare $result to send data from $sql to database ($con).
+/* Declare $res to send data from $sql to database ($con). */
 $res = mysqli_query($con, $sql);
-
+/* if doesn't have data */
 if (!mysqli_num_rows($res)) {
-    echo ("FAILURE");
-} else {
-    while ($row = mysqli_fetch_assoc($res)) {
-        
+    echo ("FAILURE"); /* response failure to app */
+} 
+else 
+    {
+         /* fetch data as associative array to $row */
+         while ($row = mysqli_fetch_assoc($res)) {
+        /* collect $row as array $data */
         $data[] = $row;
     }
-    echo json_encode($data);
+    echo json_encode($data); /* response as json format to app */
 }
-mysqli_close($con);
+mysqli_close($con); /* close connection */
     
 ?>
