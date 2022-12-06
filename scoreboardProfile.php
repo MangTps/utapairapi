423852681018888c08bae7e61587ed9ce92c9d88
MangTps <99586445+MangTps@users.noreply.github.com>
2022-12-07 02:47:14 +0700

    change to best 5 placement

--- scoreboardProfile.php
+++ scoreboardProfile.php
@@ -7,8 +7,8 @@ $sql = "SET @row_number = 0;"; /* declare variable and set value to zero */
 $sql2 = "SELECT * FROM (SELECT member.username,score.endTime,(@row_number:=@row_number+1) 
 AS row_index FROM score INNER JOIN member ON score.userId = member.userId 
 WHERE score.modeAndLevelId ='$level' ORDER BY `score`.`endTime` ASC ) AS A 
-WHERE A.username ='$username' ORDER BY A.row_index ASC LIMIT 10;";
-/*sql command to query top 10 placement in $level of $username */
+WHERE A.username ='$username' ORDER BY A.row_index ASC LIMIT 5;";
+/*sql command to query best 5 placement in $level of $username */
 
 /* send data from $sql to database ($con). */
 mysqli_query($con, $sql);
